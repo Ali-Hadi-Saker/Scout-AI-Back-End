785cb841542fed1715285e64e9454b17ac858c60
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-17 20:20:26 +0300

    added connection to database

---
+++ src/database/connection.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+
+const connectToDatabase = async ()=>{
+    mongoose.connect(process.env.DATABASE_URL)
+    console.log("connected to database");
+    
+}
+
+export default connectToDatabase
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
+import dotenv from "dotenv"
+import connectToDatabase from './database/connection.js'
 
 const app = new express()
-
+dotenv.config();
 app.use(express.json())
+
 
 app.listen(8080, ()=>{
     console.log("server running on port 8080")
+    connectToDatabase()
 })
