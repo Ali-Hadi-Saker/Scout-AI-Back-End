e618234519390af6eb757b0965bf0323e6b4bfaa
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-17 21:06:34 +0300

    added basic user schema

---
+++ src/models/user.model.js
@@ -0,0 +1,24 @@
+import mongoose, {Schema} from "mongoose";
+
+const userSchema = new Schema({
+    fname:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    role:{
+        type: String,
+        enum: ["user", "admin"],
+        default: "user"
+    }
+})
+
+export default mongoose.model("User", userSchema)
