e448eb0807e1985ee73bc34e971f6c60bbb69969
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-18 21:31:18 +0300

    started createUser controller

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -5,3 +5,19 @@ export const getUsers = async(res)=>{
     return res.send(users)
 
 }
+
+export const createUser = async (req, res)=>{
+    try {
+        const {fname, email, password} = req.body
+        if(!fname || !email || !password){
+            return res.status(400).send({message: "All field are required"})
+        }
+        const exist = User.findOne({email})
+        if(exist){
+            return res.status(400).send({message: "User already exist"})
+        }
+        
+    } catch (error) {
+        return res.status(500).send({message: error.message})
+    }
+}
