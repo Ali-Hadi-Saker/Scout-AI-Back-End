6b83b353b47801088dd717eb08776a5c3df2027e
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-18 21:18:14 +0300

    added usersRoutes to index file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from 'express'
 import dotenv from "dotenv"
 import connectToDatabase from './database/connection.js'
+import usersRoutes from "./routes/users.routes.js"
 
 const app = new express()
 dotenv.config();
+
 app.use(express.json())
 
+app.use("/users", usersRoutes)
 
 app.listen(8080, ()=>{
     console.log("server running on port 8080")
