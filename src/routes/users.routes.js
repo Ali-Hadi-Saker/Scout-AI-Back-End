2ae41045354c008975ebc04d259d25d0cc526412
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-18 22:28:40 +0300

    added loginUser route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { createUser, getUsers } from "../controllers/users.controller.js";
+import { createUser, getUsers, loginUser } from "../controllers/users.controller.js";
 
 const router = new Router()
 
 router.get("/", getUsers)
 router.post("/register", createUser )
+router.post("/login", loginUser )
+
 
 export default router
