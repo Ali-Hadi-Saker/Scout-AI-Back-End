44844452652b02d73276d8c7b36f2b1cd7d0d43f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-19 09:29:10 +0300

    finished auth.middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,7 +6,12 @@ export const authentication = (req, res, next)=>{
         if(!token){
             return res.status(401).send({message: "Unauthorized"})
         }
-        
+        jwt.verify(token, process.env.JWT_SECRET, (err, user)=>{
+            if(err) return res.status(403)
+            req.user = user
+        next()
+        })
+
     } catch (error) {
         return res.status(401).send({message: "Not authorized, token faild"})
     }
