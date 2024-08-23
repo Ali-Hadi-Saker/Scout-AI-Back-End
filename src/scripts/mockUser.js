ce78dc2f87e3c2bd6010cda3178be8786e91851b
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-23 22:56:05 +0300

    updated mockUser script

--- src/scripts/mockUser.js
+++ src/scripts/mockUser.js
@@ -1,41 +1,35 @@
-import faker from "faker";
-import userModel from "../models/user.model.js";
+import { faker } from '@faker-js/faker';
+import User from "../models/user.model.js";
 
-export const createMockUser = async (req, res) => {
-    try {
-        const mockUser = new userModel({
-            fname: faker.name.firstName(),
-            email: faker.internet.email(),
-            password: faker.internet.password(),
-            role: "user",
-            profilePicture: faker.image.avatar(),
-            cars: [
+const mockUser = new User({
+    fname: faker.person.firstName(),
+    email: faker.internet.email(),
+    password: faker.internet.password(),
+    role: "user",
+    profilePicture: faker.image.avatar(),
+    cars: [
+        {
+            carId: faker.string.uuid(),
+            status: faker.helpers.randomize(["active", "inactive"]),
+            batteryLevel: faker.number.int({ min: 0, max: 100 }),
+            cameraFeed: faker.internet.url(),
+            location: {
+                latitude: faker.location.latitude(),
+                longitude: faker.location.longitude(),
+            },
+            images: [
                 {
-                    carId: faker.datatype.uuid(),
-                    status: faker.random.arrayElement(["active", "inactive"]),
-                    batteryLevel: faker.datatype.number({ min: 0, max: 100 }),
-                    cameraFeed: faker.internet.url(),
-                    location: {
-                        latitude: faker.address.latitude(),
-                        longitude: faker.address.longitude(),
-                    },
-                    images: [
-                        {
-                            imageId: faker.datatype.uuid(),
-                            imageURL: faker.image.imageUrl(),
-                            objectDetectionResults: {
-                                objectName: faker.random.word(),
-                                confidence: faker.datatype.float({ min: 0, max: 1 }),
-                            }
-                        }
-                    ]
+                    imageId: faker.string.uuid(),
+                    imageURL: faker.image.url(),
+                    objectDetectionResults: {
+                        objectName: faker.commerce.productName(),
+                        confidence: faker.number.float({ min: 0, max: 1 }),
+                    }
                 }
             ]
-        });
+        }
+    ]
+});
 
-        const savedUser = await mockUser.save();
-        res.status(201).json({ message: 'Mock user created', user: savedUser });
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to create mock user', error });
-    }
-};
+await mockUser.save();
+console.log('Mock user created successfully!');
