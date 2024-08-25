dab2dbbfa1ea19f3bfa1836a4eafbb4f5b70298f
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-25 22:51:24 +0300

    added delete user route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createUser, getUsers, loginUser, updateUserName } from "../controllers/users.controller.js";
+import { createUser, deleteUser, getUsers, loginUser, updateUserName } from "../controllers/users.controller.js";
 
 const router = new Router()
 
@@ -7,6 +7,7 @@ router.get("/", getUsers)
 router.post("/register", createUser )
 router.post("/login", loginUser )
 router.post("/updateName", updateUserName)
+router.delete("/deleteUser/:id", deleteUser)
 
 
 export default router
