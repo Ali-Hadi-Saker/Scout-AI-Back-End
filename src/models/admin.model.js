acfcdf47f47901b0ad59f5bf14608096d618634a
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-25 22:42:54 +0300

    added admin model

--- src/models/admin.model.js
+++ src/models/admin.model.js
@@ -1,5 +1,19 @@
 import mongoose, {Schema} from "mongoose";
 
-const adminSchema = new Schema({})
+const adminSchema = new Schema({
+    fname: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: "admin"
+})
 
 export default mongoose.model("Admin", adminSchema)
