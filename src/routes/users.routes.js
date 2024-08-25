0bf65ab46d48646fb8f5ecb036bae1302a2c1870
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-25 22:20:34 +0300

    added updateUserName route

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { createUser, getUsers, loginUser } from "../controllers/users.controller.js";
+import { createUser, getUsers, loginUser, updateUserName } from "../controllers/users.controller.js";
 
 const router = new Router()
 
 router.get("/", getUsers)
 router.post("/register", createUser )
 router.post("/login", loginUser )
+router.post("/updateName", updateUserName)
 
 
 export default router
