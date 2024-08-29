4488d346f439cf1fa14e6dfc4d3b2b18d0201713
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-29 12:48:58 +0300

    updated webSocket function to start connection with esp32

--- src/servers/webSocket.js
+++ src/servers/webSocket.js
@@ -4,7 +4,18 @@ export const initializedWebSocketServre = (server) =>{
 
     const wss = new WebSocketServer({server})
 
-    wss.on('Connection', (message)=>{
+    let esp32Socket = null
+    let flutterSocket = null
+
+    wss.on('Connection', (ws)=>{
+        ws.on('message', (message)=>{
+
         consle.log(`Received ${message}`)
+
+        if (message === 'ESP32_CONNECTED'){
+            esp32Socket = ws
+        }
+        
+        })
     })
 }
