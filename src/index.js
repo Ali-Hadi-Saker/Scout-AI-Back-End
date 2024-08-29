cded36e72f138593363c652582ca1082d06d775b
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-29 13:58:03 +0300

    initialzed webSocket server in index.js file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv"
 import connectToDatabase from './database/connection.js'
 import usersRoutes from "./routes/users.routes.js"
 import cors from 'cors'
+import { initializedWebSocketServre } from './servers/webSocket.js'
+
 const app = new express()
 dotenv.config();
 const corsOptions = {
@@ -17,7 +19,9 @@ app.use(express.json())
 
 app.use("/users", usersRoutes)
 
-app.listen(8080, ()=>{
+const server = app.listen(8080, ()=>{
     console.log("server running on port 8080")
     connectToDatabase()
 })
+
+initializedWebSocketServre(server)
