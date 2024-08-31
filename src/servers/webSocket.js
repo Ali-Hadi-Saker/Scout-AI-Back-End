2492efc0cd174fe9cd56c5d635e35058356e73d4
Ali-Hadi-Saker <alihady530@gmail.com>
2024-08-31 12:21:52 +0300

    updated websocket server to convert message received from flutter from bianry to string

--- src/servers/webSocket.js
+++ src/servers/webSocket.js
@@ -10,30 +10,33 @@ export const initializedWebSocketServer = (server) => {
         console.log('New client connected');
 
         ws.on('message', (message) => {
-            // if (message instanceof Buffer) {
-            //     // Handle binary data (e.g., video frames)
-            //     console.log('Received binary data of length:', message.length);
-            //     if (flutterSocket) {
-            //         flutterSocket.send(message);
-            //     }
-            // } else {
-            //     // Handle text messages
-            //     console.log(`Received text: ${message}`);
-                if (message === 'ESP32_CONNECTED') {
-                    esp32Socket = ws;
-                    console.log('ESP32 connected !!');
-                } else if (message === 'FLUTTER_CONNECTED') {
-                    flutterSocket = ws;
-                    console.log('Flutter connected !!');
-                }
+            let messageString = '';
 
-                if (ws === flutterSocket && esp32Socket) {
-                    esp32Socket.send(message);
-                }
-                if (ws === esp32Socket && flutterSocket) {
-                    flutterSocket.send(message);
-                }
+            if (message instanceof Buffer) {
+                // If the message is a Buffer, convert it to a string
+                messageString = message.toString('utf8');
+            } else if (typeof message === 'string') {
+                // If the message is already a string, use it directly
+                messageString = message;
+            }
+
+            console.log(`Received text: ${messageString}`);
             
+            if (messageString === 'ESP32_CONNECTED') {
+                esp32Socket = ws;
+                console.log('ESP32 connected !!');
+
+            } else if (messageString === 'FLUTTER_CONNECTED') {
+                flutterSocket = ws;
+                console.log('Flutter connected !!');
+            }
+
+            if (ws === flutterSocket && esp32Socket) {
+                esp32Socket.send(messageString);
+            }
+            if (ws === esp32Socket && flutterSocket) {
+                flutterSocket.send(messageString);
+            }
         });
 
         ws.on('close', () => {
